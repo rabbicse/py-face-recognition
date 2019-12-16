9356c7ce9f5f1362b1bdd4f52eefcb12c4a46b61
Rabbi <rabbi.se@gmail.com>
2019-12-17 00:46:31 +0600

    Added simple face search web application.

---
+++ face_search/face_search/face_detector.py
@@ -0,0 +1,224 @@
+import glob
+import os
+import time
+from multiprocessing.pool import ThreadPool
+
+import cv2
+import numpy as np
+from face_search.retina_face.retinaface import RetinaFace
+
+# count = 1
+
+# detector = RetinaFace(retina_face_model, 0, 0, 'net3')
+
+# test_img = './test_data/weeding.jpg'
+
+
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+
+    return timed
+
+
+def drawText(image, text):
+    font_scale = 0.55
+    margin = 5
+    thickness = 2
+    color = (255, 255, 255)
+
+    size = cv2.getTextSize(text, FONT, font_scale, thickness)
+
+    text_width = size[0][0]
+    text_height = size[0][1]
+    line_height = text_height + size[1] + margin
+
+    x = image.shape[1] - margin - text_width
+    y = margin + size[0][1] + line_height
+
+    cv2.putText(image, text, (x, y), FONT, font_scale, color, thickness)
+
+
+class FaceDetector:
+    thresh = 0.8
+    scales = [1280, 1920]
+    retina_face_model = os.path.abspath('models/retinaface-R50/R50')
+    face_detector = RetinaFace(retina_face_model, 0, 0, 'net3')
+
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+    def process_image(self, image_path):
+        # read image from file
+        img = cv2.imread(image_path)
+
+        # get image shape
+        im_shape = img.shape
+
+        target_size, max_size = self.scales
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+
+        # if im_size_min>target_size or im_size_max>max_size:
+        im_scale = float(target_size) / float(im_size_min)
+        # prevent bigger axis from being more than max_size:
+        if np.round(im_scale * im_size_max) > max_size:
+            im_scale = float(max_size) / float(im_size_max)
+
+        print('im_scale', im_scale)
+
+        faces, landmarks = self.face_detector.detect(img, self.thresh, scales=[im_scale], do_flip=False)
+
+        if faces.any():
+            print('find', faces.shape[0], 'faces')
+            for i in range(faces.shape[0]):
+                # print('score', faces[i][4])
+                box = faces[i].astype(np.int)
+                # color = (255,0,0)
+                color = (0, 0, 255)
+                cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
+                if landmarks is not None:
+                    landmark5 = landmarks[i].astype(np.int)
+                    # print(landmark.shape)
+                    for l in range(landmark5.shape[0]):
+                        color = (0, 0, 255)
+                        if l == 0 or l == 3:
+                            color = (0, 255, 0)
+                        cv2.circle(img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+
+        cv2.imwrite('out.jpg', img)
+        return img
+
+
+    def process_images(self, image_path):
+        # read image from file
+        img = cv2.imread(image_path)
+
+        # get image shape
+        im_shape = img.shape
+
+        target_size, max_size = self.scales
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+
+        # if im_size_min>target_size or im_size_max>max_size:
+        im_scale = float(target_size) / float(im_size_min)
+        # prevent bigger axis from being more than max_size:
+        if np.round(im_scale * im_size_max) > max_size:
+            im_scale = float(max_size) / float(im_size_max)
+
+        print('im_scale', im_scale)
+
+        faces, landmarks = self.face_detector.detect(img, self.thresh, scales=[im_scale], do_flip=False)
+
+        if faces.any():
+            print('find', faces.shape[0], 'faces')
+            for i in range(faces.shape[0]):
+                # print('score', faces[i][4])
+                box = faces[i].astype(np.int)
+                # color = (255,0,0)
+                color = (0, 0, 255)
+                cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
+                if landmarks is not None:
+                    landmark5 = landmarks[i].astype(np.int)
+                    # print(landmark.shape)
+                    for l in range(landmark5.shape[0]):
+                        color = (0, 0, 255)
+                        if l == 0 or l == 3:
+                            color = (0, 255, 0)
+                        cv2.circle(img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+
+        cv2.imwrite('output_data/{}'.format(image_path.split('/')[-1]), img)
+        # cv2.imshow('Images', img)
+
+        # Hit 'q' on the keyboard to quit!
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+    def process_frames(self, img):
+        # get image shape
+        im_shape = img.shape
+
+        target_size, max_size = self.scales
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+
+        # if im_size_min>target_size or im_size_max>max_size:
+        im_scale = float(target_size) / float(im_size_min)
+        # prevent bigger axis from being more than max_size:
+        if np.round(im_scale * im_size_max) > max_size:
+            im_scale = float(max_size) / float(im_size_max)
+
+        print('im_scale', im_scale)
+
+        faces, landmarks = self.face_detector.detect(img, self.thresh, scales=[im_scale], do_flip=False)
+
+        if faces.any():
+            print('find', faces.shape[0], 'faces')
+            for i in range(faces.shape[0]):
+                # print('score', faces[i][4])
+                box = faces[i].astype(np.int)
+                # color = (255,0,0)
+                color = (0, 0, 255)
+                cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
+                if landmarks is not None:
+                    landmark5 = landmarks[i].astype(np.int)
+                    # print(landmark.shape)
+                    for l in range(landmark5.shape[0]):
+                        color = (0, 0, 255)
+                        if l == 0 or l == 3:
+                            color = (0, 255, 0)
+                        cv2.circle(img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+
+            cv2.imshow('Images', img)
+
+
+@timeit
+def recognize_images():
+    with FaceDetector() as face_rec:
+        test_images = []
+        for f in glob.glob('test_data/*.jpg'):
+            test_images.append(f)
+
+        for im in test_images:
+            face_rec.process_images(im)
+
+        with ThreadPool(1) as pool:
+            pool.map(face_rec.process_images, test_images)
+
+
+def recognize_from_webcam():
+    webcam = cv2.VideoCapture(0)
+    with FaceDetector() as face_detector:
+        while True:
+            # Grab a single frame of video
+            ret, frame = webcam.read()
+
+            face_detector.process_frames(frame)
+
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+if __name__ == '__main__':
+    recognize_images()
+    # recognize_from_webcam()
--- face_search/face_search/views.py
+++ face_search/face_search/views.py
@@ -1,8 +1,36 @@
+import os
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .face_detector import FaceDetector
+
+fd = FaceDetector()
 
 
 def home_page(request):
     context = {'title': 'Hello World',
                'content': 'Hello, We are working'}
+    if request.method == 'POST' and request.FILES['image']:
+        myfile = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        context['uploaded_file_url'] = uploaded_file_url
+        fd.process_image(os.path.join(settings.MEDIA_ROOT, myfile.name))
+
+    return render(request, 'index.html', context)
+
+
+def upload(request):
+    print('ok...')
+    context = {}
+    if request.method == 'POST' and request.FILES['image']:
+        print('ok...1')
+        myfile = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
     return render(request, 'index.html', context)
