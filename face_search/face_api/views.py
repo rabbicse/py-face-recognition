15822cfa3fb41a03f5569c1784596698a4b1b2fd
Rabbi <rabbi.se@gmail.com>
2020-01-01 00:59:06 +0600

    Updated code with rest api development

--- face_search/face_api/views.py
+++ face_search/face_api/views.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.parsers import FileUploadParser
@@ -5,19 +8,19 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, permissions
-
-# Create your views here.
 from .serializers import ImageSerializer
 
+from .face_detector import FaceDetector
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+fd = FaceDetector()
 
 
+# Create your views here.
 class FileUploadView(APIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = ImageSerializer
     parser_class = (FileUploadParser,)
+
     # renderer_classes = [TemplateHTMLRenderer]
     # template_name = 'index.html'
 
@@ -25,10 +28,19 @@ class FileUploadView(APIView):
         return Response({'status': 'OK'}, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
+        response = {}
         file_serializer = ImageSerializer(data=request.data)
 
         if file_serializer.is_valid():
             file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+            img_path = file_serializer.data['image']
+            print(img_path)
+            full_img_path = os.path.join(settings.MEDIA_ROOT, img_path.split('/')[-1])
+            print(settings.MEDIA_ROOT)
+            print('Full img path: {}'.format(full_img_path))
+            img_src = fd.process_image(full_img_path)
+            response['img_src'] = img_src.decode('utf-8')
+            response['image'] = img_path
+            return Response(response, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- face_search/static/js/image_process.js
+++ face_search/static/js/image_process.js
@@ -1,12 +1,39 @@
-$(function () {
-    console.log("Hello!");
+$("#btnSubmit").click(function(event){
+    //stop submit the form, we will post it manually.
+    event.preventDefault();
+
+    // Get form
+    var form = $('#imgForm')[0];
+
+    // Create an FormData object
+    var data = new FormData(form);
+
+    $.ajax({
+        url: '/face/image/',
+        type: 'POST',
+        data: data,
+        enctype: 'multipart/form-data',
+        processData: false,  // Important!
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function(response) {
+            $("#uploaded_img").attr("src",response['image']);
+            console.log(response);
+        },
+        error: function (e) {
+            console.log("ERROR : ", e);
+        }
+    });
 });
 
-$.ajax({
-url: '/face/image/',
-type: 'get',
-success: function(data){
-console.log("Hello   ..............1!");
-console.log(data);}
-});
+//function postAjaxRequest(e){
+//    $.ajax({
+//        url: '/face/image/',
+//        type: 'get',
+//        success: function(data){
+//        console.log("Hello   ..............1!");
+//        console.log(data);}
+//    });
+//}
 
